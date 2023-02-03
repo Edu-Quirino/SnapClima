7646cdba285d5c10e840db7c4e09b11d541422b5
Eduardo Quirino <edu.3532quirino@gmail.com>
2023-02-03 13:13:53 -0300

    inicio javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+[x] id="city-search-input"
+[x] id="city-search-button"
+[x] id="current-date"
+[x] id="city-name"
+[x] id="weather-icon"
+[x] id="weather-description"
+[x] id="current-temperature"
+[x] id="wind-speed"
+[x] id="feels-like-temperature"
+[x] id="currente-humidity"
+[x] id="sunrise-time"
+[x] id="sunset-time"
+*/
+
+// Interação
+const citySearchInput = document.getElementById('city-search-input');
+const citySearchButton = document.getElementById('city-search-button');
+
+//Exibição
+const currentDate = document.getElementById('current-date');
+const cityName = document.getElementById('city-name');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherDescription = document.getElementById('weather-description');
+const currentTemperature = document.getElementById('current-temperature');
+const windSpeed = document.getElementById('wind-speed');
+const feelsLikeTemperature = document.getElementById('feels-like-temperature');
+const currentHumidity = document.getElementById('currente-humidity');
+const sunriseTime = document.getElementById('sunrise-time');
+const sunsetTime = document.getElementById('sunset-time');
+
+
